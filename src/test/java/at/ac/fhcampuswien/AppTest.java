eaf8be70fa8dfe06834810832ba4dec62c8269c5
ZoeMRuf <marlene.z.ruf@gmail.com>
2022-02-25 13:36:46 +0100

    Initial commit - Maven, JUnit, .gitignore setup

---
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -0,0 +1,18 @@
+package at.ac.fhcampuswien;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void exampleTest(){
+        App myApp = new App();
+
+        String actual = myApp.welcomeMessage();
+        String expected = "Hello World! We come in Peace. The answer is 42!";
+
+        assertEquals(expected,actual);
+
+    }
+}
