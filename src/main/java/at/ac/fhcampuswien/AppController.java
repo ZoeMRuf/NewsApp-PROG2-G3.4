c8d6db39dbe53fa8024691b75b44e502389aa3f3
ZoeMRuf <marlene.z.ruf@gmail.com>
2022-03-03 17:34:34 +0100

    Exercise 1 generateMockList() finished, started on filterList

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -1,41 +1,63 @@
 package at.ac.fhcampuswien;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AppController {
-    private List<Article> articles;
+    private List<Article> articles = generateMockList(); // For us to test the Methods.
 
     //Constructor for this Class
     public AppController(){}
 
     //Methods
-    public  void setArticles(List <Article> articles){}
+    public void setArticles(List <Article> articles){}
 
     public int getArticleCount(){
         return 42;
     }
 
-    //public List<Article> getTopHeadlinesAustria(){}
+    public List<Article> getTopHeadlinesAustria(){
+        return articles;
+    }
 
-    //public  List<Article> getAllNewsBitcoin(){}
+    public List<Article> getAllNewsBitcoin(){
+        return articles;
+    }
 
     //filter that can only be used in this Class -> useful for getTopHeadlinesAustria & getAllNewsBitcoin
-    //protected static List<Article> filterList(String query, List<Article> articles){}
+    protected static List<Article> filterList(String query, List<Article> articles){
 
-    /*
+        ArrayList filteredList = new ArrayList();
+        Article a01 = new Article("Caitlin Cleary","Forecasters call for weather on Monday");
+        filteredList.add(a01);
+        return filteredList;
+    }
 
     private List<Article> generateMockList(){
 
-        Article example = new Article("Lukas","Unser Programm ist super");
-        Article example1 = new Article("Zoe","Unser Programm ist super");
-        Article example2 = new Article("Melanie","Unser Programm ist super");
+        Article a01 = new Article("Caitlin Cleary","Forecasters call for weather on Monday");
+        Article a02 = new Article("Scott Calvert","Cows lose their jobs as milk prices drop");
+        Article a03 = new Article("Sheldon Cooper","Most Earthquake Damage is Caused by Shaking");
+        Article a04 = new Article("Penny","Man Accused of Killing Lawyer Receives a New Attorney");
+        Article a05 = new Article("Barbara Bitcoin","State population to double by 2040, babies to blame");
+        Article a06 = new Article("Howard Wolowitz","Missippi's literacy program shows improvement");
+        Article a07 = new Article("Leonard Hofstadter","Breathing oxygen linked to staying alive");
+        Article a08 = new Article("me","Police arrest everyone on February 22nd");
+        Article a09 = new Article("Mama Wolowitz","Thursday is cancelled");
+        Article a10 = new Article("Santa Clause","Bridge closure date: Thursday or October");
+        Article a11 = new Article("Barry Allen","Safety meeting ends in accident");
+        Article a12 = new Article("Amy B. McClaw","Murderer says detective ruined his reputation");
+        Article a13 = new Article("john Smith","Utah Poison Control Center reminds everyone not to take poison");
+        Article a14 = new Article("Wayne Hansen","Bugs flying around with wings are flying bugs");
+        Article a15 = new Article("Princess Leia","Students Cook & Serve Grandparents");
 
-        List<Article> ArticleMockList = new java.awt.List() {};
-        ArticleMockList.add(example);
+        List<Article> MockList = new ArrayList<>();
 
-        return ArticleMockList;
+        MockList.add(a01); MockList.add(a02); MockList.add(a03); MockList.add(a04); MockList.add(a05);
+        MockList.add(a06); MockList.add(a07); MockList.add(a08); MockList.add(a09); MockList.add(a10);
+        MockList.add(a11); MockList.add(a12); MockList.add(a13); MockList.add(a14); MockList.add(a15);
+
+        return MockList;
     }
-
-     */
 
 }
--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -2,7 +2,19 @@ package at.ac.fhcampuswien;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class AppControllerTest {
 
+    @Test
+    public void filterListTest(){
+
+        AppController ctrl = new AppController();
+
+        String trueQuery = "Murderer";
+        //at.ac.fhcampuswien.AppController.filterList();
+
+
+    }
 
 }
